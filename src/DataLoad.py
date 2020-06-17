255e49a12de521f5e45b793664a04d1a4b6a5c6a
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2020-06-18 01:01:37 +0200

    Added more atributes for classification

--- src/DataLoad.py
+++ src/DataLoad.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.special import softmax
 
 
 def load_products_data():
@@ -69,3 +70,38 @@ def mean_accepted_discount(uid, sessions_df):
 def mean_rejected_discounts(uid, sessions_df):
     return sessions_df.loc[(sessions_df['user_id'] == uid) & (sessions_df['event_type_BUY_PRODUCT']
                                                               == 0)]['offered_discount'].mean()
+															  
+def fav_categories(uid, sessions_df, products_df):
+    products_id = sessions_df.loc[sessions_df['user_id'] == uid]['product_id']
+    fav = products_df.loc[products_df['product_id'].isin(products_id.tolist())]
+    fav = fav[fav.columns[3:]]
+    return fav.sum()
+
+def favourite_products(users_df, sessions_df, products_df):
+    arr = np.zeros(shape =(len(users_df['user_id']), 28))
+    j = 0
+    for i in users_df['user_id']:
+        arr[j] = softmax(fav_categories(i, sessions_df, products_df))
+        j += 1
+    fv =  fav_categories(102, sessions_df, products_df)
+    for k in range(len(fv.index.tolist())):
+        users_df[fv.index.tolist()[k]] = arr[:, k]
+    return users_df
+	
+def spendings(users_df, sessions_df, products_df):
+    spendings = []
+    for u_id in users_df['user_id']:
+        products_id = sessions_df.loc[(sessions_df['user_id'] == u_id)
+                                & (sessions_df['event_type_BUY_PRODUCT'] == 1)][['product_id', 'offered_discount']].to_numpy()       
+        spend = 0
+        for i in range(products_id.shape[0]):
+            if(np.isnan(products_id[i,0])):
+                continue
+            price = products_df.loc[products_df['product_id'] 
+                                 == products_id[i, 0]]['price'].to_numpy()[0] * (100 - products_id[i, 1])/100
+            if(not np.isnan(price)):
+                spend += price
+        spendings.append(spend)
+        
+    users_df['spendings'] = spendings
+    return users_df
